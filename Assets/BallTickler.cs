c322602dd4ffb804b2212f57193e7cf196305263
PoonMoon <jerrylandring@gmail.com>
2018-04-12 18:51:09 +0100

    Initial commit

---
+++ Assets/BallTickler.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class BallTickler : MonoBehaviour {
+
+
+	public float keyRepeatTime = 1f;
+
+	private bool repeaterClear = true;
+
+
+	
+	// Update is called once per frame
+	void Update () {
+
+		bool fire = CrossPlatformInputManager.GetButton("fire");
+
+
+		if (fire && repeaterClear ==true){
+			Debug.Log ("Cross plat Input manager KEYDOWN " + fire);
+			repeaterClear = false;
+			Invoke ("ResetRepeater",keyRepeatTime);
+		}
+
+		if (CrossPlatformInputManager.GetButtonUp("fire"))
+		{
+			repeaterClear =true;
+		}
+	}
+
+
+	private void ResetRepeater(){
+		
+		repeaterClear = true;
+		//poop
+
+	}
+
+}
